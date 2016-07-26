ead2ac095c54c1e5e3b3963415af264c7a4fe085
Reed <reed@plusreed.com>
2016-07-26 00:42:31 -0400

    serverconf boyz

--- main.js
+++ main.js
@@ -282,3 +282,16 @@ fox.on("message", function(message) {
     fox.reply(message, "Rawr x3 nuzzles how are you pounces on you you're so warm o3o notices you have a bulge o: someone's happy ;) nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you're so big :oooo rubbies more on your bulgy wolgy it doesn't stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I'm getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles");
   }
 });
+
+fox.on("message", function(message) {
+  if (message.content == config.prefix + "serverconf") {
+    if (message.author == message.server.owner) {
+      fox.reply(message, "It seems you are the owner of this server. This command is a WIP and will take options soon.");
+    } else if (message.server.owner == null) {
+      // if message.server.owner returns null, it's being run inside of a PMChannel
+      fox.reply(message, "You cannot run this inside of a PM channel.");
+    } else {
+      fox.reply("You aren't the owner of the server! Send this command from within your own server or get the server owner to run this command.");
+    }
+  }
+});
