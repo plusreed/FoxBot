65d459da664c0b43b7500df80af7099b1a1ac5aa
Reed <reed@plusreed.com>
2016-07-27 03:01:32 -0400

    Fixed the EventEmitter memory leak

--- main.js
+++ main.js
@@ -20,6 +20,10 @@ function Log(logmsg) {
 function attachToGui(foxGui) {
   // WIP
 }
+
+require('events').EventEmitter.defaultMaxListeners = Infinity
+
+
 // CLI variables
 var verString = "Fox CLI, version 0.1.1\nFox CLI is the command line interface for managing your Discord bot."; // If you would like to change the text that is said when you run "ver" in console, this is the place!
 var delimiter = config.delimiter; // Customize your CLI the way you want it!
@@ -276,6 +280,12 @@ fox.on("message", function(message) {
 fox.on("message", function(message) {
   if (message.content == config.prefix +  "owo") {
     fox.reply(message, "Rawr x3 nuzzles how are you pounces on you you're so warm o3o notices you have a bulge o: someone's happy ;) nuzzles your necky wecky~ murr~ hehehe rubbies your bulgy wolgy you're so big :oooo rubbies more on your bulgy wolgy it doesn't stop growing ·///· kisses you and lickies your necky daddy likies (; nuzzles wuzzles I hope daddy really likes $: wiggles butt and squirms I want to see your big daddy meat~ wiggles butt I have a little itch o3o wags tail can you please get my itch~ puts paws on your chest nyea~ its a seven inch itch rubs your chest can you help me pwease squirms pwetty pwease sad face I need to be punished runs paws down your chest and bites lip like I need to be punished really good~ paws on your bulge as I lick my lips I'm getting thirsty. I can go for some milk unbuttons your pants as my eyes glow you smell so musky :v licks shaft mmmm~ so musky drools all over your cock your daddy meat I like fondles Mr. Fuzzy Balls hehe puts snout on balls and inhales deeply oh god im so hard~ licks balls punish me daddy~ nyea~ squirms more and wiggles butt I love your musky goodness bites lip please punish me licks lips nyea~ suckles on your tip so good licks pre of your cock salty goodness~ eyes role back and goes balls deep mmmm~ moans and suckles");
+  }
+});
+
+fox.on("message", function(message) {
+  if(message.content == config.prefix + "hi") {
+    fox.reply(message, "Hi there! I'm **Fox**. I'm currently version **0.2**, and my prefix is " + config.prefix + ".");
   }
 });
 
